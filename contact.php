72f3f4861b5ddfbcdb0f00512192ad07c7a35b43
smitshetye <66062619+smitshetye@users.noreply.github.com>
2020-09-05 00:56:46 +0530

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,21 @@
+<?php
+// variable setting
+$firstname = $_REQUEST['First Name'];
+$lastname = $_REQUEST['Last Name'];
+$email = $_REQUEST['Email'];
+$subject = $_REQUEST['Subject'];
+$message = $_REQUEST['Message'];
+//check input fields
+if (empty($firstname) || empty($lastname) || empty($email) || empty($subject) || empty($message))
+{
+    echo "Please fill all the fields";
+}
+else
+{
+    mail("hasyakattaforyou@gmail.com", "Hasya Katta Message", $message, "From: $firstname <$email>");
+    echo "<script type='text/javascript'>alert('Your Message has been sent successfully');
+    window.history.log(-1)
+    </script>";
+}
+
+?>
